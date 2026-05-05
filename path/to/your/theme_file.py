4761615b17801763e21a9bbee28c6e6d5beb4b96
ABODUNRIN_EMMANUEL <154005961+127HIManuel@users.noreply.github.com>
2026-05-05 12:10:24 +0100

    Update PLOT_THEME to fix Plotly chart scroll interference

---
+++ path/to/your/theme_file.py
@@ -0,0 +1,12 @@
+# Updated PLOT_THEME
+
+# Set theme parameters
+PLOT_THEME = {
+    'plot_bgcolor': 'white',
+    'paper_bgcolor': 'white',
+    'font': {
+        'color': '#333'
+    },
+    'dragmode': 'select', # Setting dragmode
+    'scrollZoom': True # Enabling scroll zoom
+}
